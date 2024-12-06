43d035829712cf2a3cfe7edd48fae925a95dd394
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2024-12-06 16:57:03 -0300

    Upgrade

--- javascript/projeto_final/scripts/pesquisa.js
+++ javascript/projeto_final/scripts/pesquisa.js
@@ -1,5 +1,64 @@
-const pesquisa = document.querySelector("#search");
+// Importa o pacote mysql2
+const mysql = require('mysql2');
+
+// Configurações de conexão
+const connection = mysql.createConnection({
+  host: 'localhost', // Host onde o banco está rodando
+  user: 'root', // Seu usuário do MySQL
+  password: 'password', // Sua senha do MySQL
+  database: 'mercadopa' // Nome do banco de dados
+});
+
+// Conectar ao MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+    return;
+  }
+  console.log('Conectado ao banco de dados com ID ' + connection.threadId);
+});
+
+connection.query('SELECT nome_produtos FROM produtos', (err, results, fields) => {
+  if (err) {
+  console.error('Erro na consulta: ' + err.stack);
+  return;
+  }
+  console.log('Resultados da consulta: ', results);
+});
+
+/*const pesquisa = document.querySelector("#search");
+let caixaCodbarras = document.querySelector("#produtos")
 
 pesquisa.addEventListener('click',function pesquisar(){
     alert("Entrei na pesquisa");
-});
+    // Importa o pacote mysql2
+    const mysql = require('mysql2');
+
+    // Configurações de conexão
+    const connection = mysql.createConnection({
+      host: 'localhost', // Host onde o banco está rodando
+      user: 'root', // Seu usuário do MySQL
+      password: 'password', // Sua senha do MySQL
+      database: 'mercadopa' // Nome do banco de dados
+    });
+
+    // Conectar ao MySQL
+    connection.connect((err) => {
+      if (err) {
+        console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+        return;
+      }
+      console.log('Conectado ao banco de dados com ID ' + connection.threadId);
+    });
+
+    connection.query('SELECT * FROM produtos', (err, results, fields) => {
+      if (err) {
+      console.error('Erro na consulta: ' + err.stack);
+      return;
+      }
+      console.log('Resultados da consulta: ', results);
+    });
+        
+    caixaCodbarras.innerHTML = results;    
+});*/
+
