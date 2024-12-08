67be7636792de12f403191ca9bdf6761c746278d
azpthiago <thiagopazba@gmail.com>
2024-12-08 14:58:33 -0300

    Ajustar o cadastro de produtos

--- javascript/projeto_final/scripts/pesquisa.js
+++ javascript/projeto_final/scripts/pesquisa.js
@@ -1,133 +1,80 @@
-// Importa o pacote mysql2
-/*const mysql = require('mysql2');
-
-// Configurações de conexão
-const connection = mysql.createConnection({
-  host: 'localhost', // Host onde o banco está rodando
-  user: 'root', // Seu usuário do MySQL
-  password: '', // Sua senha do MySQL
-  database: 'mercadopa' // Nome do banco de dados
-});
-
-// Conectar ao MySQL
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
-    return;
-  }
-  console.log('Conectado ao banco de dados com ID ' + connection.threadId);
-});
-
-connection.query('SELECT nome_produtos FROM produtos', (err, results, fields) => {
-  if (err) {
-  console.error('Erro na consulta: ' + err.stack);
-  return;
-  }
-  console.log('Resultados da consulta: ', results);
-});
-connection.end();*/
-/*
-const pesquisa = document.querySelector("#search");
-let caixaCodbarras = document.querySelector("#produtos")
-
-pesquisa.addEventListener('click',function pesquisar(){
-    alert("Entrei na pesquisa");
-    // Importa o pacote mysql2
-    const mysql = require('mysql2');
-
-    // Configurações de conexão
-    const connection = mysql.createConnection({
-      host: 'localhost', // Host onde o banco está rodando
-      user: 'root', // Seu usuário do MySQL
-      password: 'password', // Sua senha do MySQL
-      database: 'mercadopa' // Nome do banco de dados
-    });
-
-    // Conectar ao MySQL
-    connection.connect((err) => {
-      if (err) {
-        console.error('Erro ao conectar ao banco de dados: ' + err.stack);
-        return;
-      }
-      console.log('Conectado ao banco de dados com ID ' + connection.threadId);
-    });
-
-    connection.query('SELECT * FROM produtos', (err, results, fields) => {
-      if (err) {
-      console.error('Erro na consulta: ' + err.stack);
-      return;
-      }
-      console.log('Resultados da consulta: ', results);
-    });
-        
-    caixaCodbarras.innerHTML = results;
-    
-    connection.end();
-});*/
-
 import express from 'express';
-import mysql from 'mysql2';
-import cors from 'cors';
+import mysql from 'mysql2/promise';
+import path from 'path';
 
 const app = express();
+
+// Configura o Express para fazer encode de dados vindos do front-end
+app.use(express.urlencoded({ extended: true }));
+
+// Configura o Express para utilizar JSON
 app.use(express.json());
-app.use(cors());
 
-
+// Configura o Express para utilizar a pasta public como raiz de arquivos estáticos
+// Ex: CSS, imagens, ícones e etc...
+app.use(express.static('../public'));
 
 // Config. do banco de dados
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: 'localhost', // Host onde o banco está rodando
   user: 'root', // Seu usuário do MySQL
-  password: '', // Sua senha do MySQL
-  database: 'mercadopa' // Nome do banco de dados
+  password: 'Dark@14756', // Sua senha do MySQL
+  database: 'mercadopa', // Nome do banco de dados
 });
 
-// Conectar ao MySQL
-db.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
-    return;
-  }
-  console.log('Conectado ao banco de dados');
+// Rota para mandar os HTML's para o cliente
+
+// HTML da home
+app.get('/', (req, res) => {
+  // Utiliza o método path.join() para criar umas string de local do arquivo
+  // a path.resolve() pega o da raiz do projeto e a string ao lado busca o html desejado
+  res.sendFile(path.join(path.resolve(), '../index.html'));
+});
+
+// HTML do cadastro
+app.get('/cadastro', (req, res) => {
+  // Utiliza o método path.join() para criar umas string de local do arquivo
+  // a path.resolve() pega o da raiz do projeto e a string ao lado busca o html desejado
+  res.sendFile(path.join(path.resolve(), '../pages/cadastro.html'));
 });
 
 //Cadastro de produtos
+app.post('/cadastrar', async (req, res) => {
+  // Captura os valores vindo do body
+  const data = req.body;
 
-app.post('/cadastrar', async(req,res)=>{
-  
-  const { codBarras_produtos, nome_produtos } = req.body;
-  //let codBarras_produtos = 110;
-  //let nome_produtos = "Aqui"
-  console.log(codBarras_produtos);
-  const query = 'INSERT INTO produtos (codBarras_produtos, nome_produtos) VALUES (?, ?)';
+  // Busca o id do mercado utilizando a chave do nome do mercado
+  // OBS: Traz todas chaves com o mesmo nome do banco então caso queira o id utiliza idmercados
+  const [mercado] = await db.query(`SELECT * FROM mercadopa.mercados WHERE nome_mercados like '%${data.mercadop}%'`);
+  console.log('ID do mercado -> ', mercado[0].idmercados);
 
-  db.query(query, [codBarras_produtos, nome_produtos], (err, res) => {
-    if (err) {
-      console.log(req);
-      res.send('OK');
-    }
-    console.log(req);
-    res.send('OK');
-  });
+  // Query Para inserir o produto no banco de dados
+  const query = 'INSERT INTO mercadopa.produtos (codBarras_produtos, nome_produtos) VALUES (?, ?)';
 
-  console.log(req);
+  try {
+    const resultsProduto = await db.query(query, [data.codbarras, data.nomep]);
+    console.log(resultsProduto[0].insertId);
+  } catch (err) {
+    res.send(err.message);
+  }
+
   res.send('OK');
 });
 
 //Buscar produtos
-app.get('/pesquisar',(req,res)=>{
-
-  let search = "SELECT * FROM produtos";
+app.get('/pesquisar', (req, res) => {
+  let search = 'SELECT * FROM produtos';
   db.query(search, (err, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.send("Tudo certo");
+      res.send('Tudo certo');
     }
   });
 
-  res.send('Beleza!')
+  res.send('Beleza!');
 });
 
-app.listen(3000);
+app.listen(3001, () => {
+  console.log(`URL: http://localhost:3001`);
+});
