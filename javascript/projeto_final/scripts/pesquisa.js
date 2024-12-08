6132a61cb9ce9d51bb953400444d63027ba599f2
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2024-12-08 14:05:23 -0300

    Update

--- javascript/projeto_final/scripts/pesquisa.js
+++ javascript/projeto_final/scripts/pesquisa.js
@@ -1,11 +1,11 @@
 // Importa o pacote mysql2
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 
 // Configurações de conexão
 const connection = mysql.createConnection({
   host: 'localhost', // Host onde o banco está rodando
   user: 'root', // Seu usuário do MySQL
-  password: 'password', // Sua senha do MySQL
+  password: '', // Sua senha do MySQL
   database: 'mercadopa' // Nome do banco de dados
 });
 
@@ -25,8 +25,9 @@ connection.query('SELECT nome_produtos FROM produtos', (err, results, fields) =>
   }
   console.log('Resultados da consulta: ', results);
 });
-
-/*const pesquisa = document.querySelector("#search");
+connection.end();*/
+/*
+const pesquisa = document.querySelector("#search");
 let caixaCodbarras = document.querySelector("#produtos")
 
 pesquisa.addEventListener('click',function pesquisar(){
@@ -59,6 +60,74 @@ pesquisa.addEventListener('click',function pesquisar(){
       console.log('Resultados da consulta: ', results);
     });
         
-    caixaCodbarras.innerHTML = results;    
+    caixaCodbarras.innerHTML = results;
+    
+    connection.end();
 });*/
 
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+// Config. do banco de dados
+const db = mysql.createConnection({
+  host: 'localhost', // Host onde o banco está rodando
+  user: 'root', // Seu usuário do MySQL
+  password: '', // Sua senha do MySQL
+  database: 'mercadopa' // Nome do banco de dados
+});
+
+// Conectar ao MySQL
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+    return;
+  }
+  console.log('Conectado ao banco de dados');
+});
+
+//Cadastro de produtos
+
+app.post('/cadastrar', async(req,res)=>{
+  
+  const { codBarras_produtos, nome_produtos } = req.body;
+  //let codBarras_produtos = 110;
+  //let nome_produtos = "Aqui"
+  console.log(codBarras_produtos);
+  const query = 'INSERT INTO produtos (codBarras_produtos, nome_produtos) VALUES (?, ?)';
+
+  db.query(query, [codBarras_produtos, nome_produtos], (err, res) => {
+    if (err) {
+      console.log(req);
+      res.send('OK');
+    }
+    console.log(req);
+    res.send('OK');
+  });
+
+  console.log(req);
+  res.send('OK');
+});
+
+//Buscar produtos
+app.get('/pesquisar',(req,res)=>{
+
+  let search = "SELECT * FROM produtos";
+  db.query(search, (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("Tudo certo");
+    }
+  });
+
+  res.send('Beleza!')
+});
+
+app.listen(3000);
